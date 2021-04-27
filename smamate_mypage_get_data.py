c85e5729d9d514e96133b46405e9e4ed02a45201
yoyoyo4 <34539592+yoyoyo4@users.noreply.github.com>
2021-04-27 10:06:24 +0900

    ver.0.1

--- smamate_mypage_get_data.py
+++ smamate_mypage_get_data.py
@@ -1,138 +1,157 @@
-import os
+'''
+スマメイトのマイページから戦績データを取得し、テキストファイルとして定期的に出力するプログラム
+入力 : スマメイトのマイページURL
+処理 : マイページにアクセスし戦績情報を抽出、計算
+出力 : マイページURL、今期レート、今期順位、今期勝利数、今期敗北数、連勝数、今期対戦数、今期勝率の各テキストファイル。本pyファイルと同じディレクトリに格納。一定秒数ごとに更新する
+'''
+
+import os, time
 
 import requests
+import PySimpleGUI as sg
 from bs4 import BeautifulSoup
 
+
+# 入力されたURLがスマメイトマイページのものかどうかをTrue/Falseで返す
+def can_access_mypage(mypage_url:str):
+	try:
+		html=requests.get(mypage_url)
+		soup = BeautifulSoup(html.text,"html.parser")
+		if "さんのユーザーページ スマメイト" in soup.title.text:
+			return True
+		else:
+			raise Exception
+	except:
+		return False
+
+
+# マイページURLを記録したテキストファイルの存在をチェック
+# ある場合は中身のURLにアクセスできることを確認してURLを返す
+# 無い場合、またはアクセスできない場合は空のテキストを返す
+def existing_mypage_URL():
+	try:
+		with open('マイページURL.txt', mode='r', encoding='UTF-8') as f:
+			mypage_url = f.readline()
+			if can_access_mypage(mypage_url):
+				return mypage_url
+			else:
+				raise Exception
+	except:
+		return ""
+
+
+# マイページ入力ウィンドウを表示し、入力URLからHTMLテキストを取得できるか確認する
+# 成功した場合、入力URLをテキストファイルに保存し、入力URLをそのまま返す
+# 失敗した場合、URL再入力を求める
+# URL修正用にする場合は、old_mypage_urlに修正前のマイページURLを入力する
+# 修正用の場合、入力ウィンドウを☓で閉じた場合に修正前のURLを返す
+def mypage_URL_input(old_mypage_url:str=""):
+	layout = [[sg.Text('スマメイトのマイページURLを入力してください\n例:https://smashmate.net/user/23240/')], 
+				[sg.Input(key='-IN-')], 
+				[sg.Button('OK', bind_return_key=True)]]
+	window = sg.Window('smamate_mypage_get_data', layout)
+	while True:
+		event, values = window.read()
+		if event == sg.WIN_CLOSED:
+			if old_mypage_url: # 修正用のウィンドウを閉じたかキャンセルした場合、元々のURLを返す
+				return old_mypage_url
+			else: # 修正用ではないウィンドウが閉じられた場合、プログラム全体を終了
+				exit()
+		elif event == "OK":
+			mypage_url = values['-IN-']
+			if can_access_mypage(mypage_url):
+				window.close()
+				with open('マイページURL.txt', mode='w', encoding='UTF-8') as w: # アクセス確認したマイページURLを出力
+					w.write(mypage_url)
+				return mypage_url
+			else:
+				sg.popup("戦績を取得できません。URLを確認して再入力してください", no_titlebar=True)
+
+
+# マイページURLにアクセスし、HTMLテキストを返す
+def fetch_mypage_text(mypage_url:str):
+	html=requests.get(mypage_url)
+	soup = BeautifulSoup(html.text,"html.parser")
+	return str(soup)
+
+
+# マイページのHTMLテキストから、出力するデータの辞書を作成して返す
+def make_data_dict(mypage_text:str):
+	record_text = mypage_text[mypage_text.find("<h2>レーティング対戦</h2>"):mypage_text.find("""<h2 class="mt-5">プロフィール</h2>""")] # 戦績だけ抽出
+	record_text = BeautifulSoup(record_text,"html.parser").get_text(strip=True).replace("\t","") # 例:レーティング対戦今期レート1357 (12035位)前日比：-17今期対戦成績43勝 55敗現在2連勝！動画化許可する
+	data_dict = {}
+	data_dict["今期レート"] = record_text[record_text.find("今期レート")+5:record_text.find("今期レート")+9]
+	if data_dict["今期レート"] == "": # 0戦状態のとき
+		data_dict = {"今期レート":"1500", "今期順位":"-", "今期勝利数":"0", "今期敗北数":"0", "連勝数":"0", "今期対戦数":"0", "今期勝率":"0%"}
+	else:
+		data_dict["今期順位"] = record_text[record_text.find("(")+1:record_text.find("位")]
+		data_dict["今期勝利数"] = record_text[record_text.find("今期対戦成績")+6:record_text.find("勝")] # ｢連勝｣の｢勝｣もあるが、より手前にある戦績の｢勝｣がヒットする
+		data_dict["今期敗北数"] = record_text[record_text.find("勝")+2:record_text.find("敗")]
+
+		winning_streak_idx = record_text.find("連勝") # ｢連勝｣の表記があれば連勝中
+		if 0 < winning_streak_idx:
+			data_dict["連勝数"] = record_text[record_text.find("現在")+2:winning_streak_idx]
+		else:
+			data_dict["連勝数"] = "0"
+
+		data_dict["今期対戦数"] = str(int(data_dict["今期勝利数"]) + int(data_dict["今期敗北数"]))
+		data_dict["今期勝率"] = str(int(round(100*int(data_dict["今期勝利数"])/int(data_dict["今期対戦数"])))) + "%"
+
+	return data_dict
+
+
+# データ辞書の各値を別々のテキストファイルに出力
+def output_data(data_dict:dict):
+	for s in data_dict.keys():
+		with open(s+'.txt', mode='w', encoding='UTF-8') as w:
+			w.write(data_dict[s])
+
+
+# アクセス先と次回更新までの秒数を表示しつつ、テキストファイルを更新し続ける
+def update_text_files_while_showing_status(mypage_url:str):
+	mypage_text = fetch_mypage_text(mypage_url)
+	soup = BeautifulSoup(mypage_text, "html.parser")
+	access_timeout_sec = 30 # この秒数ごとに更新。30未満の値には設定しないこと！
+	layout = [[sg.Text('以下のページにアクセス中\n\n' + soup.title.text + "\n" + mypage_url + "\n", key="text_access")], 
+				[sg.Text('次回更新まであと' + str(access_timeout_sec) + "秒", key="text_update")], 
+				[sg.Button('終了'), sg.Button('アクセスページ変更')]]
+	window = sg.Window('smamate_mypage_get_data', layout)
+	start_time = int(time.time())
+
+	while True:
+		event, _ = window.read(timeout=100)
+		if event in [sg.WIN_CLOSED, "終了"]: # ウィンドウを閉じたor終了を押したとき、プログラム全体を終了
+			exit()
+
+		elif event == "アクセスページ変更":
+			old_mypage_url = mypage_url
+			mypage_url = mypage_URL_input(old_mypage_url)
+			if mypage_url == old_mypage_url: # キャンセル連打で連続アクセスしないように処理
+				pass
+			else: # URLを修正して各種変数とテキストファイルを更新
+				mypage_text = fetch_mypage_text(mypage_url)
+				data_dict = make_data_dict(mypage_text)
+				output_data(data_dict)
+				soup = BeautifulSoup(mypage_text, "html.parser")
+				window['text_access'].update('以下のページにアクセス中\n\n' + soup.title.text + "\n" + mypage_url + "\n")
+
+		elif access_timeout_sec <= int(time.time()) - start_time: # 更新秒数以上経ったらテキストファイルを更新
+			mypage_text = fetch_mypage_text(mypage_url)
+			data_dict = make_data_dict(mypage_text)
+			output_data(data_dict)
+			start_time = int(time.time())
+
+		else:
+			window['text_update'].update('次回更新まで あと' + str(access_timeout_sec - (int(time.time()) - start_time)) + "秒")
+
+
+
 def main():
-	os.chdir(os.path.dirname(os.path.abspath(__file__))) # 本pyファイルのディレクトリをカレントディレクトリとする
-
-	# 戻り値1ならデータ取得を行わない
-	get_data(pre_data())
-
-
-def pre_data():
-	# HTMLを読みだす
-	# requests.getのかっこの中に書いてあるURLをご自身のスマメイトマイページに書き換えてください
-	# サンプルはほーずきのマイページURLです
-	html=requests.get("https://smashmate.net/user/23240/").text
-	soup=BeautifulSoup(html,"html.parser")
-
-	# タグを除外
-	text=soup.get_text()
-
-	# よしなに要素ごとに抽出
-	lines= [line.strip() for line in text.splitlines()]
-
-	text="\n".join(line for line in lines if line)
-
-	# いったんファイルに書き出し
-	with open('base.txt', mode='w', encoding='UTF-8') as b:
-		b.writelines(text)
-
-	# 今期レートという文言が見つかったら詳細処理、見つからない場合は0戦状態なので処理をスキップする
-	with open('base.txt', mode='r', encoding='UTF-8') as f:
-		check_word = f.readlines()
-	
-	check_word_position = [i for i, line in enumerate(check_word) if '今期レート' in line]
-	check_word_position = str(check_word_position)
-	check_word_position = check_word_position.strip('[') 
-	check_word_position = check_word_position.strip(']') 
-
-	if check_word_position == '':
-		return 0
-	else:
-		return 1
-
-
-def get_data(check_status):
-	# 0 = レートデータがない場合はデータがないというデータ作成を行う
-	# 1 = レートデータが見つかった場合はデータを作成する
-	if check_status == 0:
-		my_rate = '現在のレート：-'
-		with open('now_rate.txt', mode='w', encoding='UTF-8') as w:
-			w.write(my_rate)
-		
-		result_win_num = '現在の勝利数：-'
-		with open('now_win_num.txt', mode='w', encoding='UTF-8') as w:
-			w.write(result_win_num)
-		
-		result_lose_num = '現在の敗北数：-'
-		with open('now_lose_num.txt', mode='w', encoding='UTF-8') as w:
-			w.write(result_lose_num)
-		
-		result_total_match_num = '今期の総対戦数：-'
-		with open('now_total_match_num.txt', mode='w', encoding='UTF-8') as w:
-			w.write(result_total_match_num)
-		
-		result_my_win_rate = '現在の勝率：- ％'
-		with open('now_my_win_rate.txt', mode='w', encoding='UTF-8') as w:
-			w.write(result_my_win_rate)
-
-	else:
-		print_data()
-
-
-def print_data():
-	# base.txt読み込み
-	with open('base.txt', mode='r', encoding='UTF-8') as f:
-		mypage_data = f.readlines()
-	
-	# 今期レートの次の行要素を当てる
-	# 現在のレートを切り出しし書き込み
-	myrate_position = [i for i, line in enumerate(mypage_data) if '今期レート' in line]
-	myrate_position = str(myrate_position)
-	myrate_position = myrate_position.strip('[') 
-	myrate_position = myrate_position.strip(']') 
-	myrate_position = int(myrate_position)
-	myrate_position = myrate_position + 1
-
-	my_rate = mypage_data[myrate_position]
-	my_rate = my_rate[:4]
-	my_rate = '現在のレート：' + my_rate
-	with open('now_rate.txt', mode='w', encoding='UTF-8') as w:
-		w.write(my_rate)
-	
-	# 今期対戦成績の次の行要素を当てる
-	win_lose_position = [i for i, line in enumerate(mypage_data) if '今期対戦成績' in line]
-	win_lose_position = str(win_lose_position)
-	win_lose_position = win_lose_position.strip('[') 
-	win_lose_position = win_lose_position.strip(']') 
-	win_lose_position = int(win_lose_position)
-	win_lose_position = win_lose_position + 1
-
-	# 今期の勝ち数を切り出しし書き込み
-	my_win_num = mypage_data[win_lose_position]
-	win_position = my_win_num.index('勝')
-	my_win_num = my_win_num[:win_position]
-
-	result_win_num = '現在の勝利数：' + my_win_num
-	with open('now_win_num.txt', mode='w', encoding='UTF-8') as w:
-		w.write(result_win_num)
-
-	# 今期の負け数を切り出しし書き込み
-	my_lose_num = mypage_data[win_lose_position]
-	lose_position = my_lose_num.index('敗')
-	cut_position = win_position + 2
-	my_lose_num = my_lose_num[cut_position:lose_position]
-
-	result_lose_num = '現在の敗北数：' + my_lose_num
-	with open('now_lose_num.txt', mode='w', encoding='UTF-8') as w:
-		w.write(result_lose_num)
-
-	# 今期の対戦総数を算出し書き込み
-	total_match_num = int(my_win_num) + int(my_lose_num)
-	result_total_match_num = '今期の総対戦数：' + str(total_match_num)
-
-	with open('now_total_match_num.txt', mode='w', encoding='UTF-8') as w:
-		w.write(result_total_match_num)
-
-	# 今期の勝率を算出し書き込み
-	my_win_rate = round(( int(my_win_num) / int(total_match_num) ) * 100, 2)
-	result_my_win_rate = '現在の勝率：' + str(my_win_rate) + '％'
-
-	with open('now_my_win_rate.txt', mode='w', encoding='UTF-8') as w:
-		w.write(result_my_win_rate)
-
+	os.chdir(os.path.dirname(os.path.abspath(__file__))) # 本pyファイルのディレクトリに移動
+	mypage_url = existing_mypage_URL() # 保存されたマイページURLを読み込む
+	if not mypage_url: # 読み込めないときは新規入力
+		mypage_url = mypage_URL_input()
+	update_text_files_while_showing_status(mypage_url)
 
 if __name__ == '__main__':
     main()
